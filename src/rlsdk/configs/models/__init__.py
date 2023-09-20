ff14d0c8c6fec9a04a0f15db156aa8618d7fb6ff
zhizunjiege <2452816044@qq.com>
2023-09-20 16:02:02 +0800

    add more models and fix cqsim

--- src/rlsdk/configs/models/__init__.py
+++ src/rlsdk/configs/models/__init__.py
@@ -1,5 +1,11 @@
 from .dqn import DQN
+from .doubledqn import DoubleDQN
+from .ddpg import DDPG
+from .maddpg import MADDPG
 
 ModelConfigs = {model.__name__: model for model in [
     DQN,
+    DoubleDQN,
+    DDPG,
+    MADDPG,
 ]}
