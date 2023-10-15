5b5ab6f4e4d3b0eaf205928857cfa0f800d10e85
zhizunjiege <2452816044@qq.com>
2023-10-15 16:30:10 +0800

    fix incompatibility of deps' version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_namespace_packages
 
 setup(
     name='rlsdk',
-    version='1.0.0',
+    version='1.1.0',
     description='Software Development Kit for RLFrame',
     author='zhizunjiege',
     author_email='cjason@buaa.edu.cn',
@@ -13,9 +13,9 @@ setup(
     zip_safe=True,
     python_requires='>=3.8',
     install_requires=[
-        'grpcio>=1.48.2',
-        'grpcio-tools>=1.48.2',
-        'numpy>=1.23.2',
+        'grpcio==1.48.2',
+        'grpcio-tools==1.48.2',
+        'numpy==1.23.2',
     ],
     packages=find_namespace_packages(where='src', exclude=['tests']),
     package_dir={'': 'src'},
